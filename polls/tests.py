ab3157969098bbda25bacba6ef776abea70e97e9
corey-duffy <coreyjduffy@hotmail.com>
2024-04-14 10:07:12 +0100

    Adding test coverage

--- polls/tests.py
+++ polls/tests.py
@@ -22,3 +22,22 @@ class QuestionModelTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
+
+    def test_str(self):
+        text: str = "What's up?"
+        question = Question(question_text=text, pub_date=timezone.now())
+        self.assertEqual(str(question), text)
+
+    def test_does_not_exist(self):
+        with self.assertRaises(Question.DoesNotExist):
+            Question.objects.get(pk=1)
+
+    def test_save_question(self):
+        question_text = "What's new?"
+        pub_date = timezone.now()
+        question = Question(question_text=question_text, pub_date=pub_date)
+        question.save()
+
+        retrieved_question = Question.objects.get(pk=question.id)
+        self.assertEqual(retrieved_question.question_text, question_text)
+        self.assertEqual(retrieved_question.pub_date, pub_date)
